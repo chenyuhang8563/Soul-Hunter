7f01fb6f8966c47277cb49e49cced405563b0ee0
chenyuhang8563 <2235061910@st.usst.edu.cn>
2026-04-26 17:49:50 +0800

    test: cover book menu tab switching

---
+++ tests/test_book_menu_tabs.gd
@@ -0,0 +1,43 @@
+extends GutTest
+
+const BookMenuScene := preload("res://Scenes/UI/book_menu.tscn")
+
+var _menu: Control
+
+
+func before_each() -> void:
+	_menu = BookMenuScene.instantiate()
+	add_child_autofree(_menu)
+	_menu._ready()
+
+
+func test_default_page_is_backpack() -> void:
+	_menu._select_page("backpack")
+
+	assert_true(_menu.get_node("OpenContent/Pages/BackpackPage").visible)
+	assert_false(_menu.get_node("OpenContent/Pages/SettingsPage").visible)
+	assert_eq(_menu.get_node("OpenContent/Pages/BackpackPage/TitleLabel").text, "BackPack")
+
+
+func test_settings_tab_switches_to_settings_page() -> void:
+	_menu.get_node("OpenContent/Tabs/SettingsTab").pressed.emit()
+
+	assert_false(_menu.get_node("OpenContent/Pages/BackpackPage").visible)
+	assert_true(_menu.get_node("OpenContent/Pages/SettingsPage").visible)
+	assert_eq(_menu.get_node("OpenContent/Pages/SettingsPage/TitleLabel").text, "Settings")
+
+
+func test_backpack_tab_switches_back_to_backpack_page() -> void:
+	_menu.get_node("OpenContent/Tabs/SettingsTab").pressed.emit()
+	_menu.get_node("OpenContent/Tabs/BackpackTab").pressed.emit()
+
+	assert_true(_menu.get_node("OpenContent/Pages/BackpackPage").visible)
+	assert_false(_menu.get_node("OpenContent/Pages/SettingsPage").visible)
+	assert_eq(_menu.get_node("OpenContent/Pages/BackpackPage/TitleLabel").text, "BackPack")
+
+
+func test_unknown_page_falls_back_to_backpack() -> void:
+	_menu._select_page("unknown")
+
+	assert_true(_menu.get_node("OpenContent/Pages/BackpackPage").visible)
+	assert_false(_menu.get_node("OpenContent/Pages/SettingsPage").visible)
